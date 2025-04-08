f90cc56aa19393be69d1a49c79f3ad5b3e0157b6
trunghieu200903 <nghiemhieu200903@gmail.com>
2025-04-08 19:37:38 +0700

    sửa

--- seed_data/user_service_db.sql
+++ seed_data/user_service_db.sql
@@ -2,9 +2,11 @@ CREATE DATABASE user_service_db;
 USE user_service_db;
 
 -- Tạo bảng TaiKhoan
-CREATE TABLE PhanQuyen (
-    MaQuyen INT AUTO_INCREMENT PRIMARY KEY,
-    TenQuyen VARCHAR(15) NOT NULL UNIQUE
+CREATE TABLE TaiKhoan (
+    MaTaiKhoan INT AUTO_INCREMENT PRIMARY KEY,
+    TenDangNhap VARCHAR(50) UNIQUE NOT NULL,
+    MatKhau VARCHAR(255) NOT NULL,
+    LoaiQuyen INT NOT NULL DEFAULT 2 -- 0: Quản lý, 1: Nhân viên, 2: Khách hàng
 );
 
 -- Tạo bảng NguoiDung
@@ -13,20 +15,7 @@ CREATE TABLE NguoiDung (
     TenNguoiDung VARCHAR(50) NOT NULL,
     DiaChi TEXT,
     Email VARCHAR(50) UNIQUE NOT NULL,
-    SoDienThoai VARCHAR(15) UNIQUE NOT NULL
+    SoDienThoai VARCHAR(15) UNIQUE NOT NULL,
+    MaTaiKhoan INT UNIQUE NOT NULL,
+    FOREIGN KEY (MaTaiKhoan) REFERENCES TaiKhoan(MaTaiKhoan) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
--- Tạo bảng TaiKhoan
-CREATE TABLE TaiKhoan (
-    MaTaiKhoan INT AUTO_INCREMENT PRIMARY KEY,
-    MatKhau VARCHAR(255) NOT NULL,
-    MaNguoiDung INT UNIQUE NOT NULL,
-    MaQuyen INT NOT NULL DEFAULT 2, -- Mặc định là khách hàng
-    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (MaQuyen) REFERENCES PhanQuyen(MaQuyen) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- Giá trị quyền:
--- 0 - Quản lý
--- 1 - Nhân viên
--- 2 - Khách hàng (mặc định khi đăng ký)
